6a8874dfedbd263f417feeef3cea3929e102d917
Evan Sharp <Evan Sharp>
2017-01-31 18:36:16 -0700

    closure test

--- al.js
+++ al.js
@@ -1,2 +1,15 @@
 var ayy = "lmao";
 console.log(ayy);
+
+(function closureTest(x) {
+	console.log("fancy closure: ");
+	for(i=0; i<x; i++) {
+		(function(y) {
+			console.log(y);
+		}(i));
+	}
+	console.log("pleb code: ");
+	for(i=0; i<x; i++) {
+		console.log(i);
+	}
+}(10));
